704fae94920c34ed8f3b46eea9c0d27c03d59ff8
VoxWave <victor_bankowski@hotmail.com>
2015-04-02 15:49:03 +0300

    added a test for BibtexFormatter

---
+++ ohtu-miniproj/src/test/java/com/unknownpotato/ohtu/miniproj/io/BibtexFormatterTest.java
@@ -0,0 +1,14 @@
+package com.unknownpotato.ohtu.miniproj.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BibtexFormatterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
