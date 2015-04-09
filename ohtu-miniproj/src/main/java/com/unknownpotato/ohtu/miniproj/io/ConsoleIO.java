ca341226f3c76ae814a4ae416417ccc271b75467
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-04-09 15:36:22 +0300

    IO javadoc and changes

--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/ConsoleIO.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/ConsoleIO.java
@@ -5,10 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Input and output class for console interface
+ */
 @Component
 public class ConsoleIO implements IO {
 
+    /**
+     * Scanner to read input from System.in
+     */
     private Scanner scanner;
+    /**
+     * History of prints made with this io.
+     */
     private List<String> prints;
 
     public ConsoleIO() {
@@ -18,13 +27,17 @@ public class ConsoleIO implements IO {
 
     @Override
     public String readLine(String prompt) {
-        System.out.print(prompt + " ");
+        String print = prompt + " ";
+        System.out.print(print);
+        prints.add(print);
         return scanner.nextLine();
     }
 
     @Override
     public int readInt(String prompt) {
-        System.out.print(prompt + " ");
+        String print = prompt + " ";
+        System.out.print(print);
+        prints.add(print);
         return Integer.parseInt(scanner.nextLine());
     }
 
--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/IO.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/IO.java
@@ -2,9 +2,30 @@ package com.unknownpotato.ohtu.miniproj.io;
 
 import java.util.List;
 
+/**
+ * IO interface
+ */
 public interface IO {
+    /**
+     * Read a line from input.
+     * @param prompt to be printed
+     * @return read line
+     */
     String readLine(String prompt);
+    /**
+     * Read an integer from input
+     * @param prompt to be printed
+     * @return read integer
+     */
     int readInt(String prompt);
+    /**
+     * Print a line to output
+     * @param line to be printed
+     */
     void print(String toPrint);
+    /**
+     * Get everything printed in print()-method as an arraylis
+     * @return all printed lines
+     */
     List<String> getPrints();
 }
