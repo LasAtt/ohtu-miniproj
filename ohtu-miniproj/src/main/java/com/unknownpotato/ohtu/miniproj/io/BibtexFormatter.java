e521789f8b0769790187b174dbef13ad8f9725f6
VoxWave <victor_bankowski@hotmail.com>
2015-04-16 16:20:39 +0300

    added Article type support to BibtexFormater.java

--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/BibtexFormatter.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/BibtexFormatter.java
@@ -10,16 +10,25 @@ import com.unknownpotato.ohtu.miniproj.domain.ReferenceType;
  */
 public class BibtexFormatter {
 
-	public static String convertReference(Reference ref) {
+	public static String convertReference(Reference ref) throws Exception {
 		BibtexFormatter formatter = new BibtexFormatter();
 		ReferenceType type = ref.getType();
 
 		switch (type) {
 		case BOOK:
 			return formatter.formatBook(ref);
-
+		case ARTICLE:
+			return formatter.formatArticle(ref);
 		}
 
+		throw new Exception("Reference could not be formatted because the reference type is not supported");
+	}
+
+	private String formatArticle(Reference ref) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("@Article{");
+		builder.append(ref.getName());
+		builder.append(",\n");
 		return null;
 	}
 
@@ -28,6 +37,11 @@ public class BibtexFormatter {
 		builder.append("@Book{");
 		builder.append(ref.getName());
 		builder.append(",\n");
+		writeFields(ref, builder);
+		return builder.toString();
+	}
+
+	private void writeFields(Reference ref, StringBuilder builder) {
 		String value = "";
 		for (String field : ref.getFieldKeys()) {
 			value = ref.getField(field);
@@ -39,6 +53,5 @@ public class BibtexFormatter {
 			}
 		}
 		builder.append("}\n");
-		return builder.toString();
 	}
 }
