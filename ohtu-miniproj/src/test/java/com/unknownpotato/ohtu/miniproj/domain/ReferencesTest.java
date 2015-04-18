35a080498ff47f32be3d6516948225f5f69fb6f8
joopeti <pyry.joona@helsinki.fi>
2015-04-18 20:05:46 +0300

    nimigenerointiparannuksia ja testejä

--- ohtu-miniproj/src/test/java/com/unknownpotato/ohtu/miniproj/domain/ReferencesTest.java
+++ ohtu-miniproj/src/test/java/com/unknownpotato/ohtu/miniproj/domain/ReferencesTest.java
@@ -9,21 +9,36 @@ import static org.junit.Assert.*;
 public class ReferencesTest {
 
     public References refs;
+    public Reference ref;
     @Before
     public void setUp() {
         refs = new References();
-    }
-
-    @Test
-    public void deleteReferenceWorksWithCorrectName() {
         HashMap<String, String> fields = new HashMap<String, String>();
         fields.put("author", "Victor Bankowski");
         fields.put("title", "a Dive into the Rust Programming Language");
         fields.put("publisher", "Unknownpotato publishing");
         fields.put("year", "2051");
-        refs.addReference(Reference.createReference(ReferenceType.BOOK, "joo98", fields));
+        ref = Reference.createReference(ReferenceType.BOOK, "joo98", fields);
+    }
+
+    @Test
+    public void deleteReferenceWorksWithCorrectName() {
+        refs.addReference(ref);
         refs.deleteReference("joo98");
         assertEquals(null, refs.getReference("joo98"));
         assertEquals(0, refs.getReferences().size());
+    }
+    
+    @Test
+    public void addReferenceWorks(){
+        refs.addReference(ref);
+        assertEquals(ref, refs.getReference("joo98"));
+        assertEquals(1, refs.getReferences().size());
+    }
+    
+    @Test
+    public void getReferenceGivesNullForNonexistentReference(){
+        refs.addReference(ref);
+        assertEquals(null, refs.getReference("nonexistent"));
     }
 }
