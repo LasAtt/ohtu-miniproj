14104c814bf88250be92fc67e74605c5aadf3248
VoxWave <victor_bankowski@hotmail.com>
2015-04-23 18:03:33 +0300

    umlautit kesken. kommitoin koska lähdetään pois ryhmätyötilasta

--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/BibtexFormatter.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/io/BibtexFormatter.java
@@ -1,7 +1,10 @@
 package com.unknownpotato.ohtu.miniproj.io;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.unknownpotato.ohtu.miniproj.domain.Reference;
@@ -21,7 +24,10 @@ public class BibtexFormatter {
 	 * @param ref
 	 * @return String formatted in bibtex format
 	 */
+
+	
 	public static String convertReference(Reference ref) {
+		Map<Character, String> umlauts = createUmlautMap();
 		ReferenceType type = ref.getType();
 
 		StringBuilder builder = new StringBuilder();
@@ -35,7 +41,7 @@ public class BibtexFormatter {
 				.forEach(f -> {
 					builder.append(f);
 					builder.append(" = \"");
-					builder.append(ref.getField(f));
+					builder.append(replaceUmlauts(ref.getField(f), umlauts));
 					builder.append("\",\n");
 				});
 
@@ -43,11 +49,24 @@ public class BibtexFormatter {
 		return builder.toString();
 	}
 	
-	public static String replaceUmlauts(String origin){
-		return "";
+	public static String replaceUmlauts(String origin, Map<Character, String> umlauts){
+		StringBuilder builder = new StringBuilder();
+		Arrays.asList(origin.toCharArray()).forEach(c -> {
+			
+		});
+		
+		return builder.toString();
 	}
 	
 	public static List<String> convertReferences(List<Reference> reflist){
 		return reflist.stream().map(r -> convertReference(r)).collect(Collectors.toList());
+	}
+	
+	public static Map<Character, String> createUmlautMap() {
+		Map<Character, String> umlauts = new HashMap<Character, String>();
+		umlauts.put('ä', "\\\"{a}");
+		umlauts.put('ö', "\\\"{o}");
+		umlauts.put('å', "\\r{a}");
+		return umlauts;
 	}
 }
