439075467b7cbef5e98e59f71d6cfbc8c2cac411
joopeti <pyry.joona@helsinki.fi>
2015-04-23 17:50:11 +0300

    refaktorointia

--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/domain/ReferenceUtils.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/domain/ReferenceUtils.java
@@ -7,6 +7,7 @@ package com.unknownpotato.ohtu.miniproj.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
@@ -17,15 +18,56 @@ public final class ReferenceUtils {
 
     private ReferenceUtils() {
     }
-    
+
     /**
      * Generates name for reference in according to it's fields
+     *
      * @param name
      * @param fields
-     * @return 
+     * @return
      */
+    public static String generateReferenceName(String name, Map<String, String> fields) {
+        String generated = "";
+        if (!fields.get("author").isEmpty() && !fields.get("year").isEmpty()) {
+            return generateNameFromAuthorAndYear(fields);
+        } else {
+            return generateFailSafeName(fields);
+        }
+    }
 
+    /**
+     * Generates name from remaining field values if author or year fields are
+     * empty.
+     *
+     * @return
+     */
+    private static String generateFailSafeName(Map<String, String> fields) {
+        Random r = new Random();
+        String generatedName = "";
+        
+        // append two chars from each field until maximum length
+        for (String value : fields.values()) {
+            if (value != null && generatedName.length() < 8) {
+                generatedName += value.substring(0, Math.min(value.length(), 2));
+            }
+        }
+        // append random chars to get length for name
+        while(generatedName.length() < 5){
+            generatedName += (char)(r.nextInt(26) + 'a');
+        }
+        
+        return generatedName;
+    }
 
+    private static String generateNameFromAuthorAndYear(Map<String, String> fields) {
+        String generatedName = fields.get("author");
+
+        generatedName = generatedName.substring(generatedName.lastIndexOf(" ") + 1);
+        generatedName = generatedName.toLowerCase();
+        generatedName += fields.get("year").substring(2);
+
+        return generatedName;
+    }
 
     /**
      * Filters references by tag
@@ -39,9 +81,10 @@ public final class ReferenceUtils {
                 .filter(s -> s.getTags().contains(tag))
                 .collect(Collectors.toList());
     }
-    
+
     /**
      * Filters references by type
+     *
      * @param refs List of references to be filtered
      * @param type Tag to filter by
      * @return Filtered list
