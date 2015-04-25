b017f26ef59ec01af5ab45f36fe96b209c41e671
Teemu Salminen <teemujsalminen@gmail.com>
2015-04-25 15:01:30 +0300

    fixed a bug in the field validator method and added javadocs to the related methods

--- ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/domain/FieldValidator.java
+++ ohtu-miniproj/src/main/java/com/unknownpotato/ohtu/miniproj/domain/FieldValidator.java
@@ -3,11 +3,16 @@ package com.unknownpotato.ohtu.miniproj.domain;
 import org.codehaus.plexus.util.StringUtils;
 
 /**
- *
- * @author Teemu
+ * A class for validating the user's input to a given field.
  */
 public class FieldValidator {
-
+    
+/**
+     * Makes sure that the user's input to the specified field is valid.
+     *
+     * @param field the specified field
+     * @param input the user's own input to the field
+     */
     public static boolean validate(String field, String input) {
         switch (field) {
             case ("year"):
